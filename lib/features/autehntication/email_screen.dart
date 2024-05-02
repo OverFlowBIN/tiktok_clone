4e666e98c1b3bc57e64472fd43e50b46ea351870
OverFlowBIN <87470206+OverFlowBIN@users.noreply.github.com>
2024-05-02 20:23:23 +0900

    AUTHENTICATION(Email Screen 2)

--- lib/features/autehntication/email_screen.dart
+++ lib/features/autehntication/email_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/autehntication/password_screen.dart';
 import 'package:tiktok_clone/features/autehntication/widgets/form_button.dart';
 
 class EmailScreen extends StatefulWidget {
@@ -13,7 +14,7 @@ class EmailScreen extends StatefulWidget {
 class _UsernameScreenState extends State<EmailScreen> {
   final TextEditingController _usernameController = TextEditingController();
 
-  String _username = '';
+  String _email = '';
 
   @override
   void initState() {
@@ -22,7 +23,7 @@ class _UsernameScreenState extends State<EmailScreen> {
     // 해당 리스너를 통해 지속적으로 _username을 업데이트합니다.
     _usernameController.addListener(() {
       setState(() {
-        _username = _usernameController.text;
+        _email = _usernameController.text;
       });
     });
   }
@@ -36,6 +37,7 @@ class _UsernameScreenState extends State<EmailScreen> {
   }
 
   void _onNextTap() {
+    if (_email.isEmpty) return;
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (context) => const EmailScreen(),
@@ -43,50 +45,88 @@ class _UsernameScreenState extends State<EmailScreen> {
     );
   }
 
+  String? _isEmailValid() {
+    if (_email.isEmpty) return null;
+    final regExp = RegExp(
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    if (!regExp.hasMatch(_email)) {
+      return "Email not valid";
+    }
+    return null;
+  }
+
+  void _onScffoldTap() {
+    // FocusScope를 이용해서 키보드를 내리는 방법입니다.
+    FocusScope.of(context).unfocus();
+  }
+
+  void _onSubmit() {
+    if (_email.isEmpty || _isEmailValid() != null) return;
+    // Navigator.of(context).push 와 Navigator.push의 차이점은 무엇인가요?
+    // Navigator.of(context).push는 context를 사용하여 push를 합니다.
+    // Navigator.push는 context를 사용하지 않고 push를 합니다.
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const PasswordScreen(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Sign up',
+    return GestureDetector(
+      onTap: _onScffoldTap,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Sign up',
+          ),
         ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: Sizes.size40),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Gaps.v40,
-            const Text(
-              'Wait is your email?',
-              style: TextStyle(
-                fontSize: Sizes.size20,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            Gaps.v16,
-            TextField(
-              controller: _usernameController,
-              decoration: InputDecoration(
-                hintText: 'Email',
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.grey.shade400,
-                  ),
-                ),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.grey.shade400,
-                  ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: Sizes.size40),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Gaps.v40,
+              const Text(
+                'Wait is your email?',
+                style: TextStyle(
+                  fontSize: Sizes.size20,
+                  fontWeight: FontWeight.w700,
                 ),
               ),
-              cursorColor: Theme.of(context).primaryColor,
-            ),
-            Gaps.v16,
-            GestureDetector(
-                onTap: _onNextTap,
-                child: FormButton(disabled: _username.isEmpty))
-          ],
+              Gaps.v16,
+              TextField(
+                controller: _usernameController,
+                keyboardType: TextInputType.emailAddress,
+                // onSubmitted 대신 onEditingComplete를 사용하면 매게변수 값 없이 호출할 수 있다.
+                onEditingComplete: _onSubmit,
+                autocorrect: false,
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  errorText: _isEmailValid(),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                ),
+                cursorColor: Theme.of(context).primaryColor,
+              ),
+              Gaps.v28,
+              GestureDetector(
+                  onTap: _onSubmit,
+                  child: FormButton(
+                      disabled: _email.isEmpty || _isEmailValid() != null))
+            ],
+          ),
         ),
       ),
     );

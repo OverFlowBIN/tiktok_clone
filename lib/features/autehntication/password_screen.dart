576e8923e1de02a06cb41fd75048f22593ccc9d8
OverFlowBIN <87470206+OverFlowBIN@users.noreply.github.com>
2024-05-04 21:01:09 +0900

    AUTHENTICATION(Password Screen)

--- lib/features/autehntication/password_screen.dart
+++ lib/features/autehntication/password_screen.dart
@@ -1,4 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/autehntication/birthday_screen.dart';
+import 'package:tiktok_clone/features/autehntication/widgets/form_button.dart';
 
 class PasswordScreen extends StatefulWidget {
   const PasswordScreen({super.key});
@@ -8,8 +15,169 @@ class PasswordScreen extends StatefulWidget {
 }
 
 class _PasswordScreenState extends State<PasswordScreen> {
+  final TextEditingController _passwordController = TextEditingController();
+
+  String _password = '';
+  bool _obscureText = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // 해당 리스너를 통해 지속적으로 _username을 업데이트합니다.
+    _passwordController.addListener(() {
+      setState(() {
+        _password = _passwordController.text;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    // 컨트롤러를 dispose해서 메모리 누수를 방지합니다.
+    _passwordController.dispose();
+    // super.initState()와 다르게 super.dispose()는 마지막에 호출하는게 좀 더 좋은 방법이다.
+    super.dispose();
+  }
+
+  void _onNextTap() {
+    if (_password.isEmpty) return;
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const PasswordScreen(),
+      ),
+    );
+  }
+
+  bool _isPasswordValid() {
+    return _password.isNotEmpty &&
+        _password.length >= 8 &&
+        _password.length <= 20;
+  }
+
+  void _onScffoldTap() {
+    // FocusScope를 이용해서 키보드를 내리는 방법입니다.
+    FocusScope.of(context).unfocus();
+  }
+
+  void _onSubmit() {
+    if (!_isPasswordValid()) return;
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const BirthdayScreen(),
+      ),
+    );
+  }
+
+  void _onClearTap() {
+    _passwordController.clear();
+  }
+
+  void _toggleObscureText() {
+    _obscureText = !_obscureText;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return GestureDetector(
+      onTap: _onScffoldTap,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Sign up',
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: Sizes.size40),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Gaps.v40,
+              const Text(
+                'Password',
+                style: TextStyle(
+                  fontSize: Sizes.size20,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              Gaps.v16,
+              TextField(
+                controller: _passwordController,
+                // obscureText는 TextField에 입력된 텍스트를 숨기는 기능입니다.
+                obscureText: _obscureText,
+                // onSubmitted 대신 onEditingComplete를 사용하면 매게변수 값 없이 호출할 수 있다.
+                onEditingComplete: _onSubmit,
+                decoration: InputDecoration(
+                  // Row를 사용하면 최대 너비를 가지려고 하기때문에 맨 앞에 배치가 된다.
+
+                  suffix: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      GestureDetector(
+                        onTap: _onClearTap,
+                        child: FaIcon(
+                          FontAwesomeIcons.solidCircleXmark,
+                          color: Colors.grey.shade400,
+                          size: Sizes.size20,
+                        ),
+                      ),
+                      Gaps.h14,
+                      GestureDetector(
+                        onTap: _toggleObscureText,
+                        child: FaIcon(
+                          _obscureText
+                              ? FontAwesomeIcons.eye
+                              : FontAwesomeIcons.eyeSlash,
+                          color: Colors.grey.shade400,
+                          size: Sizes.size20,
+                        ),
+                      ),
+                    ],
+                  ),
+                  hintText: 'Make it strong!',
+
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                ),
+                cursorColor: Theme.of(context).primaryColor,
+              ),
+              Gaps.v10,
+              const Text('Your password must have:',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  )),
+              Gaps.v10,
+              Row(
+                children: [
+                  FaIcon(
+                    FontAwesomeIcons.circleCheck,
+                    size: Sizes.size20,
+                    color: _isPasswordValid()
+                        ? Colors.green
+                        : Colors.grey.shade400,
+                  ),
+                  Gaps.h5,
+                  const Text('8 to 20 characters')
+                ],
+              ),
+              Gaps.v28,
+              GestureDetector(
+                  onTap: _onSubmit,
+                  child: FormButton(disabled: !_isPasswordValid()))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }

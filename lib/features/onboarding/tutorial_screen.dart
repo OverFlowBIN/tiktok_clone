3701e648a9e72d0e71d6586d22253eb636558e20
OverFlowBIN <87470206+OverFlowBIN@users.noreply.github.com>
2024-05-12 20:37:08 +0900

    ONBOARDING(Tutorial Screen)

---
+++ lib/features/onboarding/tutorial_screen.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+
+import '../../constants/sizes.dart';
+
+class TutorialScreen extends StatefulWidget {
+  const TutorialScreen({super.key});
+
+  @override
+  State<TutorialScreen> createState() => _TutorialScreenState();
+}
+
+class _TutorialScreenState extends State<TutorialScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        body: const SafeArea(
+          // TabBarView의 역할은 TabBar에 정의된 Tab들을 리스트화하여 보여주는 역할을 합니다.
+          child: TabBarView(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: Sizes.size24),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Gaps.v52,
+                    Text(
+                      "Wathch cool videos!",
+                      style: TextStyle(
+                        fontSize: Sizes.size32,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Gaps.v16,
+                    Text(
+                      "Videos are personalized for you based on what you watch, like, and share.",
+                      style: TextStyle(
+                        fontSize: Sizes.size16,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: Sizes.size24),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Gaps.v52,
+                    Text(
+                      "Follow the rules of the app",
+                      style: TextStyle(
+                        fontSize: Sizes.size32,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Gaps.v16,
+                    Text(
+                      "Videos are personalized for you based on what you watch, like, and share.",
+                      style: TextStyle(
+                        fontSize: Sizes.size16,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: Sizes.size24),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Gaps.v52,
+                    Text(
+                      "Enjoy the ride",
+                      style: TextStyle(
+                        fontSize: Sizes.size32,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Gaps.v16,
+                    Text(
+                      "Videos are personalized for you based on what you watch, like, and share.",
+                      style: TextStyle(
+                        fontSize: Sizes.size16,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Container(
+            padding: const EdgeInsets.symmetric(
+              vertical: Sizes.size48,
+            ),
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TabPageSelector(
+                  color: Colors.white,
+                  selectedColor: Colors.black38,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

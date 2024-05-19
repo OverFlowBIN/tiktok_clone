fde89a00fab7558bcacf8ada0f871bd8207288fd
OverFlowBIN <87470206+OverFlowBIN@users.noreply.github.com>
2024-05-19 22:39:21 +0900

    TAB NAVIGATION(Custom NavigationBar)

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/main_navigation/widgets/nav_tab.dart';
 
 class MainNavigationNavogation extends StatefulWidget {
   const MainNavigationNavogation({super.key});
@@ -35,31 +38,44 @@ class _MainNavigationNavogationState extends State<MainNavigationNavogation> {
   Widget build(BuildContext context) {
     // CupertinoTabScaffold를 사용하게 되면, index값을 따로 수정해줄 필요가 없다.
     // Cupertino 테마를 올바르게 사용하기 위해서는 main.dart에서 MaterialApp을 CupertinoApp으로 변경해서 사용해야 한다.
-    return CupertinoTabScaffold(
-      tabBar: CupertinoTabBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(
-              // CupertinoIcons를 사용하게 되면 조금 더 IOS 처럼 표현된다
-              CupertinoIcons.house,
-              semanticLabel: "Home",
-            ),
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.all(
+            Sizes.size12,
           ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              CupertinoIcons.search,
-              semanticLabel: "Search",
-            ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              NavTab(
+                text: "Home",
+                onTap: () => _onTap(0),
+                isSelected: _selectIndex == 0,
+                icon: FontAwesomeIcons.house,
+              ),
+              NavTab(
+                text: "Discover",
+                onTap: () => _onTap(1),
+                isSelected: _selectIndex == 1,
+                icon: FontAwesomeIcons.magnifyingGlass,
+              ),
+              NavTab(
+                text: "Inbox",
+                onTap: () => _onTap(3),
+                isSelected: _selectIndex == 3,
+                icon: FontAwesomeIcons.message,
+              ),
+              NavTab(
+                text: "User",
+                onTap: () => _onTap(4),
+                isSelected: _selectIndex == 4,
+                icon: FontAwesomeIcons.user,
+              ),
+            ],
           ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              CupertinoIcons.settings,
-              semanticLabel: "Setting",
-            ),
-          ),
-        ],
+        ),
       ),
-      tabBuilder: (context, index) => screens[index],
     );
   }
 }
---
+++ lib/features/main_navigation/widgets/nav_tab.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import '../../../constants/gaps.dart';
+
+class NavTab extends StatelessWidget {
+  const NavTab(
+      {super.key,
+      required this.text,
+      required this.isSelected,
+      required this.icon,
+      required this.onTap});
+
+  final String text;
+  final bool isSelected;
+  final IconData icon;
+  final Function onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    // Expanded를 넣게되면 하위 widget에서 UI를 차지하는 Container를 꽉 채울 수 있다.
+    return Expanded(
+      child: GestureDetector(
+        onTap: () => onTap(),
+        // Container를 추가하여 버튼을 클릭할 수 있는 영역을 넓힐 수 있다.
+        child: Container(
+          color: Colors.black,
+          child: AnimatedOpacity(
+            opacity: isSelected ? 1 : 0.6,
+            duration: const Duration(microseconds: 300),
+            child: Column(
+              // mainAxisSize를 정하지 않는다면 Column이라는 세로축으로 최대한 확장하려 하기 때문에 전체를 차지하게 된다.
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                FaIcon(
+                  icon,
+                  color: Colors.white,
+                ),
+                Gaps.v5,
+                Text(
+                  text,
+                  style: const TextStyle(
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

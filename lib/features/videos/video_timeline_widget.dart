5aa0e2fae71153a54a1bffac41d39164f5d80b8b
OverFlowBIN <87470206+OverFlowBIN@users.noreply.github.com>
2024-05-29 00:35:55 +0900

    VIDEO TIMELINE(PageController) PageController

--- lib/features/videos/video_timeline_widget.dart
+++ lib/features/videos/video_timeline_widget.dart
@@ -8,6 +8,7 @@ class VedioTimelineScreen extends StatefulWidget {
 }
 
 class _VedioTimelineScreenState extends State<VedioTimelineScreen> {
+  final PageController _pageController = PageController();
   int _itemCount = 4;
 
   final List<Color> _colors = [
@@ -18,6 +19,11 @@ class _VedioTimelineScreenState extends State<VedioTimelineScreen> {
   ];
 
   void _onPageChanged(int index) {
+    _pageController.animateToPage(
+      index,
+      duration: const Duration(milliseconds: 5),
+      curve: Curves.linear,
+    );
     if (index == _itemCount - 1) {
       _itemCount = _itemCount + 4;
       _colors.addAll(
@@ -37,19 +43,21 @@ class _VedioTimelineScreenState extends State<VedioTimelineScreen> {
     // PageView를 사용하면 스크롤 가능한 화면을 만들 수 있습니다.
     // PageView는 모든 페이지를 미리 빌드하여 메모리를 많이 사용하므로 페이지가 많은 경우에는 ListView.builder를 사용하는 것이 좋습니다.
     return PageView.builder(
-        scrollDirection: Axis.vertical,
-        onPageChanged: _onPageChanged,
-        itemCount: _colors.length,
-        itemBuilder: (context, index) => Container(
-              color: _colors[index],
-              child: Center(
-                child: Text(
-                  'Page $index',
-                  style: const TextStyle(
-                    fontSize: 40,
-                  ),
-                ),
-              ),
-            ));
+      controller: _pageController,
+      scrollDirection: Axis.vertical,
+      onPageChanged: _onPageChanged,
+      itemCount: _colors.length,
+      itemBuilder: (context, index) => Container(
+        color: _colors[index],
+        child: Center(
+          child: Text(
+            'Page $index',
+            style: const TextStyle(
+              fontSize: 40,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }

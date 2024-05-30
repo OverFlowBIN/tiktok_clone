dd765fb2cbe24745c18350eba37dc0adcb283899
OverFlowBIN <87470206+OverFlowBIN@users.noreply.github.com>
2024-05-30 20:23:11 +0900

    COMMENTS SECTION(Comments) Listview.seperated

--- lib/features/videos/widgets/video_comments.dart
+++ lib/features/videos/widgets/video_comments.dart
@@ -3,6 +3,7 @@ import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import '../../../constants/gaps.dart';
 import '../../../constants/sizes.dart';
 
 class VideoComments extends StatefulWidget {
@@ -39,13 +40,72 @@ class _VideoCommentsState extends State<VideoComments> {
             )
           ],
         ),
-        body: ListView.builder(
+        body: ListView.separated(
+          padding: const EdgeInsets.symmetric(
+            vertical: Sizes.size10,
+            horizontal: Sizes.size16,
+          ),
           itemCount: 10,
-          itemBuilder: (context, index) {
-            return Container(
-              child: Text("Comment $index"),
-            );
-          },
+          separatorBuilder: (context, index) => Gaps.v20,
+          itemBuilder: (context, index) => Container(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const CircleAvatar(
+                  radius: 18,
+                  child: Text("OB"),
+                ),
+                Gaps.h8,
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'OverFlowBIN',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: Sizes.size14,
+                          color: Colors.grey.shade500,
+                        ),
+                      ),
+                      Gaps.v4,
+                      const Text(
+                          "That's not it I'm not sure about thisThat's not it I'm not sure about thisThat's not it I'm not sure about thisThat's not it I'm not sure about thisThat's not it I'm not sure about this"),
+                    ],
+                  ),
+                ),
+                Column(
+                  children: [
+                    FaIcon(
+                      FontAwesomeIcons.heart,
+                      size: Sizes.size20,
+                      color: Colors.grey.shade500,
+                    ),
+                    Gaps.v2,
+                    Text(
+                      '1.2M',
+                      style: TextStyle(
+                        color: Colors.grey.shade500,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.white,
+          child: Row(
+            children: [
+              CircleAvatar(
+                radius: 18,
+                backgroundColor: Colors.grey.shade500,
+                foregroundColor: Colors.white,
+                child: const Text("OB"),
+              ),
+            ],
+          ),
         ),
       ),
     );

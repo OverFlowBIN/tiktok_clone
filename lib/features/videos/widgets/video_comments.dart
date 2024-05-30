8a8c3229ede4bb8337cfb5ba60e231296ca3fc7e
OverFlowBIN <87470206+OverFlowBIN@users.noreply.github.com>
2024-05-30 21:13:18 +0900

    COMMENTS SECTION(Text Input Action) TextField(suffixIcon, minLines: null)

--- lib/features/videos/widgets/video_comments.dart
+++ lib/features/videos/widgets/video_comments.dart
@@ -12,8 +12,24 @@ class VideoComments extends StatefulWidget {
 }
 
 class _VideoCommentsState extends State<VideoComments> {
+  bool _isWriting = false;
+
   void _onClosePressed() {
     Navigator.of(context).pop();
+  }
+
+  void _stopWritting() {
+    FocusScope.of(context).unfocus();
+
+    setState(() {
+      _isWriting = false;
+    });
+  }
+
+  void _onStartWriting() {
+    setState(() {
+      _isWriting = true;
+    });
   }
 
   @override
@@ -42,108 +58,155 @@ class _VideoCommentsState extends State<VideoComments> {
           ],
         ),
         // TextFiled를 사용할때 입력창이 키보드 위로 따라오게 하기 위해 Stack과 positioned를 사용한다.
-        body: Stack(
-          children: [
-            ListView.separated(
-              padding: const EdgeInsets.symmetric(
-                vertical: Sizes.size10,
-                horizontal: Sizes.size16,
-              ),
-              itemCount: 10,
-              separatorBuilder: (context, index) => Gaps.v20,
-              itemBuilder: (context, index) => Container(
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const CircleAvatar(
-                      radius: 18,
-                      child: Text("OB"),
-                    ),
-                    Gaps.h8,
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+        body: GestureDetector(
+          onTap: _stopWritting,
+          child: Stack(
+            children: [
+              ListView.separated(
+                padding: const EdgeInsets.symmetric(
+                  vertical: Sizes.size10,
+                  horizontal: Sizes.size16,
+                ),
+                itemCount: 10,
+                separatorBuilder: (context, index) => Gaps.v20,
+                itemBuilder: (context, index) => Container(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const CircleAvatar(
+                        radius: 18,
+                        child: Text("OB"),
+                      ),
+                      Gaps.h8,
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'OverFlowBIN',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: Sizes.size14,
+                                color: Colors.grey.shade500,
+                              ),
+                            ),
+                            Gaps.v4,
+                            const Text(
+                                "That's not it I'm not sure about thisThat's not it I'm not sure about thisThat's not it I'm not sure about thisThat's not it I'm not sure about thisThat's not it I'm not sure about this"),
+                          ],
+                        ),
+                      ),
+                      Column(
                         children: [
+                          FaIcon(
+                            FontAwesomeIcons.heart,
+                            size: Sizes.size20,
+                            color: Colors.grey.shade500,
+                          ),
+                          Gaps.v2,
                           Text(
-                            'OverFlowBIN',
+                            '1.2M',
                             style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: Sizes.size14,
                               color: Colors.grey.shade500,
                             ),
                           ),
-                          Gaps.v4,
-                          const Text(
-                              "That's not it I'm not sure about thisThat's not it I'm not sure about thisThat's not it I'm not sure about thisThat's not it I'm not sure about thisThat's not it I'm not sure about this"),
                         ],
-                      ),
-                    ),
-                    Column(
-                      children: [
-                        FaIcon(
-                          FontAwesomeIcons.heart,
-                          size: Sizes.size20,
-                          color: Colors.grey.shade500,
-                        ),
-                        Gaps.v2,
-                        Text(
-                          '1.2M',
-                          style: TextStyle(
-                            color: Colors.grey.shade500,
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            ),
-            Positioned(
-              bottom: 0,
-              width: size.width,
-              child: BottomAppBar(
-                color: Colors.white,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: Sizes.size16,
-                    vertical: Sizes.size10,
-                  ),
-                  child: Row(
-                    children: [
-                      CircleAvatar(
-                        radius: 18,
-                        backgroundColor: Colors.grey.shade500,
-                        foregroundColor: Colors.white,
-                        child: const Text("OB"),
-                      ),
-                      Gaps.h10,
-                      // Row안의 Expanded는 Row의 남은 공간을 차지하게 된다.
-                      Expanded(
-                        child: TextField(
-                          cursorColor: Theme.of(context).primaryColor,
-                          decoration: InputDecoration(
-                            hintText: 'Add a comment...',
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(
-                                Sizes.size12,
-                              ),
-                              borderSide: BorderSide.none,
-                            ),
-                            filled: true,
-                            fillColor: Colors.grey.shade200,
-                            contentPadding: const EdgeInsets.symmetric(
-                              horizontal: Sizes.size10,
-                              vertical: Sizes.size12,
-                            ),
-                          ),
-                        ),
-                      ),
+                      )
                     ],
                   ),
                 ),
               ),
-            )
-          ],
+              Positioned(
+                bottom: 0,
+                width: size.width,
+                child: BottomAppBar(
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: Sizes.size16,
+                      vertical: Sizes.size10,
+                    ),
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 18,
+                          backgroundColor: Colors.grey.shade500,
+                          foregroundColor: Colors.white,
+                          child: const Text("OB"),
+                        ),
+                        Gaps.h10,
+                        // Row안의 Expanded는 Row의 남은 공간을 차지하게 된다.
+                        Expanded(
+                          child: SizedBox(
+                            height: Sizes.size48,
+                            child: TextField(
+                              onTap: _onStartWriting,
+                              expands: true,
+                              // minLines: null, maxLines: null로 설정하면 TextField의 높이가 자동으로 조절된다.
+                              minLines: null,
+                              maxLines: null,
+                              textInputAction: TextInputAction.newline,
+                              cursorColor: Theme.of(context).primaryColor,
+                              decoration: InputDecoration(
+                                hintText: 'Add a comment...',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(
+                                    Sizes.size12,
+                                  ),
+                                  borderSide: BorderSide.none,
+                                ),
+                                filled: true,
+                                fillColor: Colors.grey.shade200,
+                                contentPadding: const EdgeInsets.symmetric(
+                                  horizontal: Sizes.size12,
+                                ),
+                                // suffixIcon은 TextField 오른쪽에 위치하게 된다.
+                                // 반대로 prefixIcon은 TextField 왼쪽에 위치하게 된다.
+                                suffixIcon: Padding(
+                                  padding: const EdgeInsets.only(
+                                    right: Sizes.size8,
+                                  ),
+                                  child: Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      FaIcon(
+                                        FontAwesomeIcons.at,
+                                        color: Colors.grey.shade900,
+                                      ),
+                                      Gaps.h10,
+                                      FaIcon(
+                                        FontAwesomeIcons.gift,
+                                        color: Colors.grey.shade900,
+                                      ),
+                                      Gaps.h10,
+                                      FaIcon(
+                                        FontAwesomeIcons.faceSmile,
+                                        color: Colors.grey.shade900,
+                                      ),
+                                      Gaps.h10,
+                                      if (_isWriting)
+                                        GestureDetector(
+                                          onTap: _stopWritting,
+                                          child: FaIcon(
+                                            FontAwesomeIcons.circleArrowUp,
+                                            color:
+                                                Theme.of(context).primaryColor,
+                                          ),
+                                        )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );

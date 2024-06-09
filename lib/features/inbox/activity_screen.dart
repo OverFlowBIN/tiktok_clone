03448b817960a44d5b31a8ae28410e2f2eada874
OverFlowBIN <87470206+OverFlowBIN@users.noreply.github.com>
2024-06-09 14:48:33 +0900

    INBOX(Dismissible)

--- lib/features/inbox/activity_screen.dart
+++ lib/features/inbox/activity_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -10,14 +11,19 @@ class ActivityScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("All activity"),
-        ),
-        body: ListView(
-          padding: const EdgeInsets.symmetric(horizontal: Sizes.size20),
-          children: [
-            Gaps.v14,
-            Text(
+      appBar: AppBar(
+        title: const Text("All activity"),
+      ),
+      // Dismissible은 스와이프로 삭제할 수 있는 위젯
+      // 스와이프 방향에 따라 다른 방식의 이벤트를 처리할 수 있음
+      body: ListView(
+        // padding: const EdgeInsets.symmetric(horizontal: Sizes.size20),
+        padding: const EdgeInsets.symmetric(horizontal: 0),
+        children: [
+          Gaps.v14,
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: Sizes.size16),
+            child: Text(
               'New',
               style: TextStyle(
                 fontSize: Sizes.size14,
@@ -25,9 +31,38 @@ class ActivityScreen extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            Gaps.v14,
-            ListTile(
-              contentPadding: EdgeInsets.zero,
+          ),
+          Gaps.v14,
+          Dismissible(
+            key: const Key('x'),
+            background: Container(
+              // Container안에 alignment를 주면 child의 위치를 조정할 수 있음
+              alignment: Alignment.centerLeft,
+              color: Colors.green,
+              child: const Padding(
+                padding: EdgeInsets.only(left: Sizes.size10),
+                child: FaIcon(
+                  FontAwesomeIcons.checkDouble,
+                  color: Colors.white,
+                  size: Sizes.size40,
+                ),
+              ),
+            ),
+            secondaryBackground: Container(
+              // Container안에 alignment를 주면 child의 위치를 조정할 수 있음
+              alignment: Alignment.centerRight,
+              color: Colors.red,
+              child: const Padding(
+                padding: EdgeInsets.only(right: Sizes.size10),
+                child: FaIcon(
+                  FontAwesomeIcons.trashCan,
+                  color: Colors.white,
+                  size: Sizes.size40,
+                ),
+              ),
+            ),
+            child: ListTile(
+              // contentPadding: EdgeInsets.zero,
               leading: Container(
                 width: Sizes.size52,
                 decoration: BoxDecoration(
@@ -73,7 +108,9 @@ class ActivityScreen extends StatelessWidget {
               trailing: const FaIcon(FontAwesomeIcons.chevronRight,
                   size: Sizes.size16),
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }

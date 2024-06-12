6286d1c6260dfacf8695f951d0668849730b3438
OverFlowBIN <87470206+OverFlowBIN@users.noreply.github.com>
2024-06-12 22:11:37 +0900

    CHAT(AnimatedList part Two)

--- lib/features/inbox/chat_screen.dart
+++ lib/features/inbox/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/inbox/chat_detail_screen.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -13,17 +14,77 @@ class _ChatScreenState extends State<ChatScreen> {
   final GlobalKey<AnimatedListState> _key = GlobalKey<AnimatedListState>();
 
   final List<int> _items = [];
+  final _duration = const Duration(
+    milliseconds: 200,
+  );
 
   void _addItem() {
     if (_key.currentState != null) {
       _key.currentState!.insertItem(
         _items.length,
-        duration: const Duration(
-          milliseconds: 200,
-        ),
+        duration: _duration,
       );
       _items.add(_items.length);
     }
+  }
+
+  void _deleteItem(int index) {
+    if (_key.currentState != null) {
+      _key.currentState!.removeItem(
+        index,
+        (context, animation) => SizeTransition(
+          sizeFactor: animation,
+          child: Container(
+            color: Colors.red,
+            child: _makeTile(index),
+          ),
+        ),
+        duration: _duration,
+      );
+      _items.removeAt(index);
+    }
+  }
+
+  void _onChatTap() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const ChatDetailScreen(),
+      ),
+    );
+  }
+
+  Widget _makeTile(int index) {
+    return ListTile(
+      onLongPress: () => _deleteItem(index),
+      onTap: _onChatTap,
+      key: UniqueKey(),
+      leading: const CircleAvatar(
+        radius: Sizes.size28,
+        foregroundImage: NetworkImage(
+            "https://avatars.githubusercontent.com/u/87470206?v=4"),
+        child: Text("YB"),
+      ),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            "OverFlowBIN ($index)",
+            style: const TextStyle(
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          Text(
+            "2:16 PM",
+            style: TextStyle(
+              color: Colors.grey.shade500,
+              fontSize: Sizes.size12,
+            ),
+          ),
+        ],
+      ),
+      subtitle: const Text("Don't forget tio make video"),
+    );
   }
 
   @override
@@ -46,41 +107,12 @@ class _ChatScreenState extends State<ChatScreen> {
         padding: const EdgeInsets.symmetric(
           vertical: Sizes.size10,
         ),
+        // item이 추가되거나 삭제될때 itemBuilder function은 작동하게 된다.₩
         itemBuilder: (context, index, animation) {
           return FadeTransition(
             opacity: animation,
-            child: SizeTransition(
-              sizeFactor: animation,
-              child: ListTile(
-                key: UniqueKey(),
-                leading: const CircleAvatar(
-                  radius: Sizes.size28,
-                  foregroundImage: NetworkImage(
-                      "https://avatars.githubusercontent.com/u/87470206?v=4"),
-                  child: Text("YB"),
-                ),
-                title: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text(
-                      "OverFlowBIN ($index)",
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Text(
-                      "2:16 PM",
-                      style: TextStyle(
-                        color: Colors.grey.shade500,
-                        fontSize: Sizes.size12,
-                      ),
-                    ),
-                  ],
-                ),
-                subtitle: const Text("Don't forget tio make video"),
-              ),
-            ),
+            child:
+                SizeTransition(sizeFactor: animation, child: _makeTile(index)),
           );
         },
       ),
